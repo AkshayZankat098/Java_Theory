3209c4e21993ab73eaaa14c770cbb359a554c96b
AkshayZankat098 <akshayzankat999@gmail.com>
2025-06-13 18:15:16 +0530

    13th June from company

---
+++ src/seleniumwebdriver/xpaththeory/xpath.java
@@ -0,0 +1,21 @@
+package seleniumwebdriver.xpaththeory;
+
+public class xpath {
+
+//Xpath : Syntax for finding any element on the we page using XML path (Location of web elements) expression (DOM structure).
+//CopyFullXpath : /html/body/app-root/app-menu/div/div/div/div/mx-blocked-users/div[3]/div[1]/div/div/form/div[1]/div[2]/div/mx-server-pick-list-button/form/div/div/div[1]/input
+//CopyXpath : //*[@id="AutoBlockUserID"]
+
+//1) Absolute Xpath / Full Xpath :  
+//2) Relative Xpath / Partial Xpath : 
+
+//Syntax of relative Xpath parth01 : //tagname[@attribute='Value']
+//Example : //input[@name='txtUsername'] && //*[@name='txtUsername'] (*= Regular expression and represent of all the tagnames or tags) 
+
+//Methods Present in Xpath :: 
+//or, and, contains(), starts-with(), text(), chained xpath
+
+
+
+
+}
